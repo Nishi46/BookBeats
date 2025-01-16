a208a336f614b1da80d75aa42a80bf02890b999a
Nishi Kantamneni <nk475@cornell.edu>
2025-01-15 16:55:53 -0800

    cleaning code

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,35 @@
 document.getElementById('bookForm').addEventListener('submit', function(event) {
-  event.preventDefault();  
+    event.preventDefault();
 
-  const query = document.getElementById('bookQuery').value;
+    const query = document.getElementById('bookQuery').value;
 
-  if (query.trim() === "") {
-      alert("Please enter a book title!");
-      return;
-  }
+    if (query.trim() === "") {
+        alert("Please enter a book title!");
+        return;
+    }
 
-  fetch(`/search_book?q=${query}`)
-      .then(response => response.json())
-      .then(data => {
-          if (data.error) {
-              alert(data.error);  
-          } else {
+    fetch(`/search_book?q=${query}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                alert(data.error);
+            } else {
 
-              const recommendationsContainer = document.getElementById('recommendations');
-              recommendationsContainer.innerHTML = `
+                const recommendationsContainer = document.getElementById('recommendations');
+                recommendationsContainer.innerHTML = `
                   <h2>Recommended Songs</h2>
               `;
 
-              data.recommended_songs.forEach((song, index) => {
-                  const songElement = document.createElement('p');
-                  songElement.innerHTML = `<strong>${index + 1}. </strong><a href="${song.song_url}" target="_blank">${song.song_name}</a>`;
-                  recommendationsContainer.appendChild(songElement);
-              });
-              recommendationsContainer.style.display = 'block';
-          }
-      })
-      .catch(error => {
-          console.error('Error:', error);
-          alert("There was an error processing your request.");
-      });
+                data.recommended_songs.forEach((song, index) => {
+                    const songElement = document.createElement('p');
+                    songElement.innerHTML = `<strong>${index + 1}. </strong><a href="${song.song_url}" target="_blank">${song.song_name}</a>`;
+                    recommendationsContainer.appendChild(songElement);
+                });
+                recommendationsContainer.style.display = 'block';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert("There was an error processing your request.");
+        });
 });
